e31ad95407444ffac27255fba6c8569cb836f9b1
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-09-27 14:55:07 +0800

    Created different traits the ticket can have

---
+++ app/Http/Modules/Ticket/Fetchable.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Htpt\Modules\Ticket;
+
+trait Fetchable
+{
+    /**
+     * Returns the ticket type attribute value
+     *
+     * @param string $value
+     * @return string
+     */
+    public function getTicketTypeAttribute($value)
+	{
+		return ucwords($value);
+	}
+
+    /**
+     * Returns the ticket details attribute value
+     *
+     * @param string $value
+     * @return string
+     */
+	public function getDetailsAttribute($value)
+	{
+		return ucwords($value);
+	}
+
+    /**
+     * Returns the ticket name attribute value
+     *
+     * @param string $value
+     * @return string
+     */
+	public function getTicketNameAttribute($value)
+	{
+		return ucwords($value);
+	}
+
+    /**
+     * Returns the equivalent status for open ticket
+     *
+     * @return string
+     */
+	protected function getOpenStatus()
+	{
+		return self::OPEN_STATUS;
+	}
+
+    /**
+     * Returns the equivalent status for closed ticket
+     *
+     * @return string
+     */
+	protected function getClosedStatus()
+	{
+		return self::CLOSED_STATUS;
+	}
+
+    /**
+     * Returns the equivalent status for transferred ticket
+     *
+     * @return string
+     */
+	protected function getTransferredStatus()
+	{
+		return self::TRANSFERRED_STATUS;
+	}
+
+	/**
+	 * Returns the table linked to the ticket. Returns null if
+	 * the tag cannot be found on any of the tables
+	 *
+	 * @param string $tag
+	 * @return object
+	 */
+	public function getTagDetails($tag)
+	{
+
+		// check if the tag belongs to a workstation
+		// returns the workstation information
+		if($workstation = Workstation::isWorkstation($tag))
+		{
+			return $workstation;
+		} 
+
+		// check if the tag belongs to a item
+		// returns the item information
+		else if($item = Item::isItem($tag))  
+		{
+			return $item;
+		}
+
+		// check if the tag belongs to a room
+		// returns the room information
+		else if($room = Room::exists($tag)) 
+		{
+			return $room;
+		}
+
+		return null;
+	}
+}
---
+++ app/Http/Modules/Ticket/Mutable.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Htpt\Modules\Ticket;
+
+trait Mutable
+{
+    
+	/**
+	 * Sets the current ticket type to uppercase value
+	 *
+	 * @param string $value
+	 * @return string
+	 */
+	public function setTickettypeAttribute($value)
+	{
+		$this->attributes['type'] = ucwords($value);
+	}
+
+	/**
+	 * Sets the parent id to null
+	 * 
+	 * @return object
+	 */
+	protected function parentIsNull()
+	{
+		$this->parent_id = null;
+
+		return $this;
+	}
+	
+	/**
+	 * Set the ticket type to the argument listed. If the name exists in database
+	 * fetches the record, else it creates a new type
+	 *
+	 * @param string $type
+	 * @return object
+	 */
+	protected function setTypeTo($type)
+	{
+		$this->type_id = Type::firstOrCreate([
+			'name' => 'Maintenance'
+		])->id;
+
+		return $this;
+	}
+
+	/**
+	 * Sets the ticket status to open
+	 * 
+	 * @return object
+	 */
+	protected function setStatusToOpen()
+	{
+		$this->status = self::OPEN_STATUS;
+
+		return $this;
+	}
+
+	/**
+	 * Sets the ticket status to closed
+	 * 
+	 * @return object
+	 */
+	protected function setStatusToClosed()
+	{
+		$this->status = self::CLOSED_STATUS;
+
+		return $this;
+	}
+
+	/**
+	 * Sets the ticket status to transferred
+	 * 
+	 * @return object
+	 */
+	protected function setStatusToTransferred()
+	{
+		$this->status = self::TRANSFERRED_STATUS;
+
+		return $this;
+	}
+
+	/**
+	 * Assigns the ticket to the current user
+	 * 
+	 * @return object
+	 */
+	protected function assignToCurrentUser()
+	{
+		$this->staff_id = Auth::user()->id;
+
+		return $this;
+	}
+
+	/**
+	 * Sets the details of the ticket
+	 *
+	 * @param string $details
+	 * @return object
+	 */
+	protected function withDetails($details)
+	{
+		$this->details = $details;
+
+		return $this;
+	}
+
+	/**
+	 * Sets the title of the ticket
+	 *
+	 * @param string $title
+	 * @return object
+	 */
+	protected function withTitle($title)
+	{
+		$this->title = $title;
+
+		return $this;
+	}
+}
