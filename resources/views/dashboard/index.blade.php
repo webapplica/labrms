0f941b09353951065ac6e8a59b7ed6d380cc8cfe
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-10-04 22:38:45 +0800

    Added tickets to dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -14,14 +14,19 @@
         </div> <!-- end of reservation tab -->
 	</div>
 	<div class=" col-md-6">
-		<div class="panel panel-primary">
-			<div class="panel-body">
-			    <h4 class="line-either-side text-muted">
-		      		Activity
-		      	</h4>
-				<div id="ticket-list">
-				</div>
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				<h3>Tickets</h3>
 			</div>
+			<div class="list-group">
+				@foreach($tickets as $ticket)
+				<a href="{{ url('ticket/' . $ticket->id) }}" class="list-group-item">
+					<h4 class="list-group-item-heading">{{ $ticket->title }}</h4>
+					<p class="list-group-item-text">{{ $ticket->details }}</p>
+				</a>
+				@endforeach
+			</div>
+			{{ $tickets->links() }}
         </div> <!-- end of activities tab -->
 	</div>
 	<div class="col-md-3" id="accordion" role="tablist" aria-multiselectable="true">
