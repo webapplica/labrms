98dcb343da11240f5c3b60ef068a04cbecf9ada4
Gabriel Jay Huerte <gjhuerte@gmail.com>
2018-11-12 22:28:55 +0800

    Reworked add action module for items inventory profile

--- resources/views/inventory/profile/action/create.blade.php
+++ resources/views/inventory/profile/action/create.blade.php
@@ -1,30 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6 panel panel-body panel-padding">
+<div 
+	class="container-fluid col-sm-offset-2 col-sm-8 col-md-offset-3 col-md-6 panel panel-body">
 
 	<legend>
-        <h3 class="text-muted pull-left">Item {{ $item->local_id }}: Add Activity</h3>
-        <span class="pull-right label label-info label-lg">{{ $currentDate }}</span>
-        <div class="clearfix"></div>
+		<h3 class="text-muted">Item: {{ $item->local_id }}</h3>
 	</legend>
 
 	<ul class="breadcrumb">
-		<li><a href="{{ url('inventory') }}">Item</a></li>
+		<li><a href="{{ url('item') }}">Item</a></li>
 		<li><a href="{{ url('item/' . $item->id) }}">{{ $item->local_id }}</a></li>
-		<li class="">Activity</li>
-		<li class="active">Add</li>
+		<li class="active">Action</li>
 	</ul>
 		      
 	@include('errors.alert')
 
-	{{ Form::open(['method' => 'post', 'url' => url('item/' . $item->id . '/activity/add'),  'id' => 'item-form']) }}
+    {{ Form::open(['method' => 'post', 'url' => url('item/' . $item->id . '/activity/add'),  'id' => 'item-form']) }}
+    
+        <div class="form-group">
+            <input 
+                type="checkbox"
+                name="maintenance"
+                {{ old('maintenance') || $item->isUnderMaintenance() ? 'checked' : '' }}
+                /> {{ __('Undermaintenance?') }}
+
+        </div>
+
+		<div class="form-group">
+			{{ Form::label('subject', 'Subject') }}
+			{{ Form::text('subject', old('subject'), [
+				'class' => 'form-control',
+				'placeholder' => 'Input a subject for the action done'
+			]) }}
+
+			<p class="text-danger" style="font-size: 12px;">*{{ __('label.required_field') }}</p>
+		</div>
 
 		<div class="form-group">
 			{{ Form::label('details', 'Details') }}
 
 			<p class="text-muted" style="font-size: 12px;">
-				Enter specific details of the activity done to the item.
+				This field is required to further explain the details of the action
 			</p>
 
 			{{ Form::textarea('details', old('details'), [
